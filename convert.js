6ea16356bf52c3c52bc6e871a95f37965f40af82
snake97123 <73522198+snake97123@users.noreply.github.com>
2023-09-24 11:07:59 +0900

    相互変換機能の追加

--- convert.js
+++ convert.js
@@ -1,13 +1,30 @@
 document.querySelector('#convertButton').addEventListener('click', () => {
-  const csv = document.querySelector('#csvInput').value;
-  const json = csvToJson(csv);
-  document.querySelector('#jsonOutput').value = JSON.stringify(json, null, 2);
-});
+  const input = document.querySelector('#csvOrJsonInput').value;
+  let output = '';
+
+  if(isJson(input)) {
+    const jsonObj = JSON.parse(input);
+    output = jsonToCsv(jsonObj);
+  } else {
+    output = csvToJson(input);
+  }
+
+  document.querySelector('#jsonOrCsvOutput').value = output;
+})
 
 document.querySelector('#clearButton').addEventListener('click', () => {
-  document.querySelector('#csvInput').value = '';
-  document.querySelector('#jsonOutput').value = '';
+  document.querySelector('#csvOrJsonInput').value = '';
+  document.querySelector('#jsonOrCsvOutput').value = '';
 });
+
+function isJson(str) {
+    try {
+      JSON.parse(str);
+    } catch(e) {
+      return false;
+    }
+    return true;
+}
 
 function csvToJson(csv) {
   const lines = csv.split('\n');
@@ -16,11 +33,24 @@ function csvToJson(csv) {
 
   for(let i = 1; i < lines.length - 1; i++) {
     const obj = {};
-    const currentline = lines[i].split(",");
+    const currentLine = lines[i].split(",");
     for(let j = 0; j < headers.length; j++) {
-      obj[headers[j]] = currentline[j];
+      obj[headers[j]] = currentLine[j];
     }
     result.push(obj);
   }
   return result;
+}
+
+function jsonToCsv(json) {
+  const header = Object.keys(json[0]);
+  const headerString = header.join(',');
+
+  const replacer = (key, value) => value ?? '';
+  const rowItems = json.map((row) => 
+    header.map((fieldName) => JSON.stringify(row[fieldName], replacer)).join(',')
+  );
+  const csv = [headerString, ...rowItems].join('\r\n');
+
+  return csv;
 }
